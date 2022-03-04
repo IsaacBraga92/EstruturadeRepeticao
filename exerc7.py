4f7de15682aea3d312f3463514721771bc39428d
Isaac Braga <isaacnwt@gmail.com>
2022-03-03 23:01:07 -0400

    Initial commit

--- exerc7.py
+++ exerc7.py
@@ -1,4 +1,5 @@
 #   Faça um programa que leia 5 números e informe o maior número.
+
 maior = float(input("Informe um número: "))
 for _ in range(4):
     maior = max(maior, float(input("Informe um número: ")))
