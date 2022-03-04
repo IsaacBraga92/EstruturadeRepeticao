0c8f0a68393461bfc158095c68c5677208a0a22b
Isaac Braga <isaacnwt@gmail.com>
2022-03-03 23:08:29 -0400

    Initial commit

--- exerc8.py
+++ exerc8.py
@@ -1,13 +1,7 @@
 # Faça um programa que leia 5 números e informe a soma e a média dos números.
 
-aux = 0
-soma=0
-while aux < 5:
-    aux += 1
-    n = float(input("Informe um número: "))
-    soma = soma+n
-
-media=soma/aux
-
-print(f"A soma dos números é: {soma}")
-print(f"A media dos números é {media}")
+soma = float(input("Informe um número: "))
+for aux in range(2, 6):
+    soma += float(input("Informe um número: "))
+    media = soma/aux
+    print(f"A soma dos números é {soma}, e a média é {media}.")
