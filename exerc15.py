8ae9e746c577560954ac43970990b41538b9491f
Isaac Braga <isaacnwt@gmail.com>
2022-03-05 12:34:40 -0400

    Initial commit

--- exerc15.py
+++ exerc15.py
@@ -1,3 +1,16 @@
-# A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até
-# o n−ésimo termo.
+# A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,...
+# Faça um programa capaz de gerar a série até o n−ésimo termo.
 
+numero = int(input("Qual termo deseja encontrar? "))
+ultimo = 0
+penultimo = 1
+
+if (numero ==1) or (numero ==2):
+    print("1")
+else:
+    for auxiliar in range(0,numero):
+        termo = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = termo
+        auxiliar+=1
+        print(termo, end=' ')
